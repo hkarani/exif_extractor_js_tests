fc17cf2fc65a632fd1159a0280598dde5df03547
hkarani <hezronkaranii@gmail.com>
2024-02-18 17:02:38 +0300

    Test with image urls

--- test.js
+++ test.js
@@ -4,47 +4,21 @@ const base64 = require('base64-js');
 const axios = require('axios');
 const { error } = require('console');
 
-async function extractExifData(filePath) {
-    try{
-        // Read the image file
-        const imageBinaryData = fs.readFileSync(filePath);
-
-        // Convert binary data to base64
-        const base64Data = base64.fromByteArray(imageBinaryData);
-
-        // Convert to Data URI
-        const dataUri = `data:image/jpeg;base64,${base64Data}`;
-
-        // Get the file name
-        const fileName = path.basename(filePath);
+async function extractExifData(url) {
+    try{    
 
         const datavals = {
-                "fileName": fileName,
-                "image": dataUri              
+            "imageURL": url   
+        }       
 
-        }
-
-        // Testing deployed lambda here
-        axios.post('https://osyseixnxqtiou464ibqy7bgmm0sgckc.lambda-url.us-east-1.on.aws/',datavals).
+        // Send payload in a POST request locally
+        axios.post('http://localhost:9000/2015-03-31/functions/function/invocations',datavals).
         then((resp) => {
                 console.log(resp.data);
 
-        })
-        
-
-        // Send payload in a POST request locally
-        // axios.post('http://localhost:9000/2015-03-31/functions/function/invocations',datavals).
-        // then((resp) => {
-        //         console.log(resp.data);
-
-        // })
-        
-
-       
+        })  
         
         
-        
-        // return response.data;
     } catch (error) {
         console.error('Error:', error.message);
     }
@@ -54,9 +28,8 @@ async function extractExifData(filePath) {
  * Testing function calls
  */
 
-// extractExifData('pics/Crémieux11.tiff');
-// extractExifData('pics/HMD_Nokia_8.3_5G.heif');
-//Jobagent.tiff will throw error it also thows error on jimpl
-// extractExifData('pics/Jobagent.tiff');
-// extractExifData('pics/WWL_(Polaroid)_ION230.jpg');
-extractExifData('pics/DSCN0010.jpg');
+// extractExifData('https://github.com/ianare/exif-samples/raw/master/tiff/Cr%C3%A9mieux11.tiff');
+// extractExifData('https://github.com/ianare/exif-samples/raw/master/tiff/Picoawards.tiff');
+// extractExifData('https://github.com/ianare/exif-samples/blob/master/jpg/gps/DSCN0027.jpg?raw=true');
+// extractExifData('https://github.com/ianare/exif-samples/raw/master/heic/mobile/HMD_Nokia_8.3_5G.heif');
+extractExifData('https://i.pinimg.com/originals/d9/34/d0/d934d091874d26ef5db598178b12340a.jpg');
